f55bf8354e5b50dbebe4093c49aaca8758bde2fd
Shubh-2025 <samratkarmakarnaihati@gmail.com>
2025-08-25 19:49:19 +0530

    register logic done

--- backend/controllers/registerHandler.mjs
+++ backend/controllers/registerHandler.mjs
@@ -2,7 +2,17 @@ import pool from '../db.mjs';
 import errorHandler from '../error.mjs';
 
 const registerHandler = async (req, res) => {
-    res.send("ok");
+    try {
+        const user = req.body;
+        if(user){
+            await pool.query("insert into husers ( name , email , password , nationality ) values($1 , $2 , $3  , $4 ) returning uid",[user.name, user.email, user.password,user.nationality]);
+            res.status(200).json({"message":"Registered Successfully"});
+        } else {
+            res.status(400).json({"message":"Could'nt Register the user..."});
+        }
+    } catch (error) {
+        console.log(error);
+        errorHandler(req,res);
+    }
 }
-
 export default registerHandler;
