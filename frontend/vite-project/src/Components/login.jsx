8ae78511adbdc00ad2d321880dd046f0e14d4443
Debottam Kar <debottamkar2003@gmail.com>
2025-08-25 19:08:52 +0530

    login and register component done

--- frontend/vite-project/src/Components/login.jsx
+++ frontend/vite-project/src/Components/login.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 
 function login() {
+  let { register, handleSubmit, reset } = useForm();
+  function submitForm(data) {
+    console.log(data);
+    // reset();
+  }
   return (
-    <div>login</div>
-  )
+    <div>
+      <form onSubmit={handleSubmit(submitForm)}>
+        <input type="email" {...register("email")} requires />
+        <intput type="password" {...register("password")} requires />
+        <button type="submit">Login</button>
+      </form>
+      <p>Do not have account? Register</p>
+    </div>
+  );
 }
 
-export default login
+export default login;
