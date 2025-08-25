ffc494f627d92a91c1beba8298e4815cca8f13fa
Shubh-2025 <samratkarmakarnaihati@gmail.com>
2025-08-26 00:20:27 +0530

    navbar fixed

--- frontend/vite-project/src/Components/Navbar.jsx
+++ frontend/vite-project/src/Components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { FaHome } from "react-icons/fa";
 import { FaSearch } from 'react-icons/fa';
-import { FaInfoCircle } from 'react-icons/fa';
+import { FaMagic } from 'react-icons/fa';
 
 function Navber() {
   return (
-    <div className='nav' >
-    <h1><FaHome /></h1>
-    <h1><FaSearch /></h1>
-    <h1><FaInfoCircle /></h1>
+    <div className='nav' style={{padding:"10px",display:"flex",justifyContent:"space-evenly",flexWrap:"wrap"}} >
+    <FaHome size={40} style={{cursor:"pointer"}}/>
+    <FaSearch size={40} style={{cursor:"pointer"}}/>
+    <FaMagic size={40} style={{cursor:"pointer"}}/>
     </div>
     //icons
   )
