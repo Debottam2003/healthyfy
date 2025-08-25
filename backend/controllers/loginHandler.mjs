8dffdff337131d32e15f3f57ff2351a6ca30539a
Debottam Kar <debottamkar2003@gmail.com>
2025-08-25 19:58:57 +0530

    register handler done

--- backend/controllers/loginHandler.mjs
+++ backend/controllers/loginHandler.mjs
@@ -2,7 +2,25 @@ import errorHandler from "../error.mjs";
 import pool from '../db.mjs';
 
 const loginHandler = async (req, res) => {
-    res.send("ok");
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).send("Both Email and password are required");
+        } else {
+            let { rows } = await pool.query("select email, password from husers where email = $1", [email]);
+            if (rows.length === 0) {
+                return res.status(400).send("Wrong email or password");
+            }
+            else if (password === rows[0].password) {
+                return res.status(200).json({ message: "Welcome to Healthyfy again." });
+            } else {
+                return res.status(400).send("Wrong email or password");
+            }
+        }
+        res.send("ok");
+    } catch (error) {
+        errorHandler(req, res);
+    }
 }
 
 export default loginHandler;
--- backend/controllers/registerHandler.mjs
+++ backend/controllers/registerHandler.mjs
@@ -3,16 +3,20 @@ import errorHandler from '../error.mjs';
 
 const registerHandler = async (req, res) => {
     try {
+
         const user = req.body;
-        if(user){
-            await pool.query("insert into husers ( name , email , password , nationality ) values($1 , $2 , $3  , $4 ) returning uid",[user.name, user.email, user.password,user.nationality]);
-            res.status(200).json({"message":"Registered Successfully"});
+        if (!user.email || !user.password || !user.name || !user.nationality) {
+            return res.status(400).send("All fields are required");
+        }
+        if (user) {
+            await pool.query("insert into husers ( name , email , password , nationality ) values($1 , $2 , $3  , $4 ) returning uid", [user.name, user.email, user.password, user.nationality]);
+            res.status(200).json({ "message": "Registered Successfully" });
         } else {
-            res.status(400).json({"message":"Could'nt Register the user..."});
+            res.status(400).json({ "message": "Could'nt Register the user..." });
         }
     } catch (error) {
         console.log(error);
-        errorHandler(req,res);
+        errorHandler(req, res);
     }
 }
 export default registerHandler;
