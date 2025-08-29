36d71321ea34c27c9db7bb807b3d0cdf25a8b914
Debottam Kar <debottamkar2003@gmail.com>
2025-08-30 02:22:14 +0530

    cookie fixing

--- backend/controllers/loginHandler.mjs
+++ backend/controllers/loginHandler.mjs
@@ -16,7 +16,7 @@ const loginHandler = async (req, res) => {
             }
             else if (password === rows[0].password) {
                 let token = jwt.sign({ uid: rows[0].uid }, secret, { expiresIn: '10m' });
-                res.cookie('token', token, { maxAge: 20 * 60 * 1000, httpOnly: true });
+                res.cookie('token', token, { maxAge: 20 * 60 * 1000, httpOnly: true, secure: true, sameSite: 'none' });
                 return res.status(200).json({ message: "Welcome to Healthyfy again." });
             } else {
                 return res.status(400).json({ message: "Wrong email or password" });
