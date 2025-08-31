e6a979e764f21ad77f5282afa15b6f9913622c21
Debottam Kar <debottamkar2003@gmail.com>
2025-09-01 02:12:49 +0530

    scaling is done

--- backend/controllers/homeHandler.mjs
+++ backend/controllers/homeHandler.mjs
@@ -1,11 +1,12 @@
 import pool from '../db.mjs';
 import errorHandler from '../error.mjs';
 
-const homeHandler = async (req, res) => {
+let query = "SELECT r.rid, r.name, r.imageurl, r.cuisine, r.type , COUNT(l.uid) AS likes_count FROM recipes r LEFT JOIN likes l ON r.rid = l.rid GROUP BY r.rid";
+let { rows } = await pool.query(query);
+
+const homeHandler = (req, res) => {
     try {
         // console.log("homeHandler called");
-        let query = "SELECT r.rid, r.name, r.imageurl, r.cuisine, r.type , COUNT(l.uid) AS likes_count FROM recipes r LEFT JOIN likes l ON r.rid = l.rid GROUP BY r.rid";
-        let { rows } = await pool.query(query);
         // console.log(rows[0]);
         res.status(200).json(rows);
     } catch (error) {
