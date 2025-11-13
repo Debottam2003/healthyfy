a8da71b903910d8f336871b175278dd74ddbaa51
Debottam Kar <debottamkar2003@gmail.com>
2025-11-13 22:13:08 +0530

    feature added

--- backend/controllers/allGenerations.mjs
+++ backend/controllers/allGenerations.mjs
@@ -4,7 +4,7 @@ import errorHandler from "../error.mjs";
 const allGenerations = async (req, res) => {
     try {
         let { uid } = req.user;
-        let { rows } = await pool.query("select * from generations where uid = $1", [uid]);
+        let { rows } = await pool.query("select * from generations where uid = $1 order by generationid asc", [uid]);
         // Placeholder logic for user generations handler
         res.status(200).json(rows);
     } catch (error) {
