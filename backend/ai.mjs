a5915f1b74cfac2f01849f2924d9495d556ae728
Debottam Kar <debottamkar2003@gmail.com>
2026-03-31 22:01:17 +0530

    fixes

--- backend/ai.mjs
+++ backend/ai.mjs
@@ -3,4 +3,4 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
-export default { ai, Modality};
+export default { ai, Modality };
--- backend/controllers/cuisineHandler.mjs
+++ backend/controllers/cuisineHandler.mjs
@@ -3,9 +3,9 @@ import errorHandler from "../error.mjs";
 
 const cuisineHandler = async (req, res) => {
     try {
-        let {rows} = await pool.query("select cuisine FROM recipes GROUP BY cuisine LIMIT 100;");
+        let { rows } = await pool.query("select cuisine FROM recipes GROUP BY cuisine LIMIT 100;");
         res.status(200).json(rows);
-    } catch(err) {
+    } catch (err) {
         console.log(err.message);
         errorHandler(req, res);
     }
